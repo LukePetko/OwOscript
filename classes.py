43400428b11811330fe2316eea9337d14dadf2a0
Lukáš Peťko <luke.petko@gmail.com>
2024-03-18 17:09:02 +0100

    adding binary expwessions parsing and semicowon :3

--- classes.py
+++ classes.py
@@ -13,10 +13,35 @@ class FunctionDeclaration:
     def __str__(self) -> str:
         return f'name: {self.name}, args: {self.args}, body: {self.body}'
 
-class MathOperationDeclaration:
+class ReturnStatement:
+    def __init__(self, statement) -> None:
+        self.statement = statement
+    def __str__(self) -> str:
+        return f'statement {self.statement}'
+
+class BinaryOperation:
     def __init__(self, left, right, operation) -> None:
         self.left = left
         self.right = right
         self.operation = operation
     def __str__(self) -> str:
         return f'left: {self.left}, right: {self.right}, operation: {self.operation}'
+
+class NumberLiteral:
+    def __init__(self, value) -> None:
+        self.value = value
+    def __str__(self) -> str:
+        return f'value {self.value}'
+        
+class StringLiteral:
+    def __init__(self, value) -> None:
+        self.value = value
+    def __str__(self) -> str:
+        return f'value {self.value}'
+
+class VariableReference:
+    def __init__(self, value) -> None:
+        self.value = value
+    def __str__(self) -> str:
+        return f'value {self.value}'
+
--- compile.py
+++ compile.py
@@ -1,5 +1,5 @@
 import re
-from classes import VariableDeclaration, FunctionDeclaration
+from classes import BinaryOperation, NumberLiteral, ReturnStatement, StringLiteral, VariableDeclaration, FunctionDeclaration, VariableReference
 
 token_specification = [
     ('COMMENT',  r'//.*'),                # Comment
@@ -15,7 +15,8 @@ token_specification = [
     ('RBRACE',   r'\}'),                  # Right curly brace
     ('COMMA',    r','),                   # Comma
     ('NEWLINE',  r'\n'),                  # Line endings
-    ('SKIP',     r'[ \t;]+'),             # Skip over spaces and tabs
+    ('SEMI',     r';'),                   # Semicolon
+    ('SKIP',     r'[ \t]+'),              # Skip over spaces and tabs
     ('MISMATCH', r'.')                    # Any other character
 ]
 
@@ -66,7 +67,7 @@ def parse_variable_declaration(tokens):
     if assign[0] != 'ASSIGN':
         raise SyntaxError(f'Expected =, got {assign[1]}')
 
-    value = next(tokens) # TODO parse expressions
+    value = parse_binary_expression(tokens)
 
     return VariableDeclaration(name, value)
 
@@ -112,29 +113,81 @@ def parse_function_body(tokens):
         elif token[0] == 'KEYWORD':
             if token[1] == 'vaw':
                 children.append(parse_variable_declaration(tokens))
+            elif token[1] == 'wetuwn':
+                children.append(ReturnStatement(parse_binary_expression(tokens)))
         elif token[0] == 'COMMENT':
             continue
         else:
-            print(token)
+            continue
+
+def parse_binary_expression(tokens):
+    left = next(tokens)
+
+    if left[0] not in ["NUMBER", "STRING", "IDENT"]:
+        raise SyntaxError(f'Expected string, number or variable, got {left[1]}')
+
+    match left[0]:
+        case "NUMBER":
+            left = NumberLiteral(left)
+        case "STRING":
+            left = StringLiteral(left)
+        case "IDENT":
+            left = VariableReference(left)
+
+    operation = None
+    try:
+        operation = next(tokens)
+    except StopIteration:
+        return left
 
 
+    if operation[0] in ["COMMENT", "SEMI"]:
+        return left
 
+    if operation[0] != "OPERATOR":
+        raise SyntaxError(f'Expected operation, got {operation[1]}')
 
+    right = next(tokens)
+
+    if right[0] not in ["NUMBER", "STRING", "IDENT"]:
+        raise SyntaxError(f'Expected operation, got {right[1]}')
+
+    match right[0]:
+        case "NUMBER":
+            right = NumberLiteral(right)
+        case "STRING":
+            right = StringLiteral(right)
+        case "IDENT":
+            right = VariableReference(right)
+
+    return BinaryOperation(left, right, operation)
+
+def parse(tokens):
+    ast = []
+
+    for token in tokens:
+        if token[0] == "KEYWORD":
+            match(token[1]):
+                case "vaw":
+                    ast.append(parse_variable_declaration(tokens))
+                case "functiwoon":
+                    ast.append(parse_function_declaration(tokens))
+        elif token[0] == "COMMENT":
+            continue
+
+    return ast
+
+ast = []
 
 tokens = []
 
-with open("template.owo", "r") as f:
+with open("testfile.owo", "r") as f:
     tokens = iter(lexer(f.read()))
 
 # tokens = iter(lexer("vaw x = 5; // this is a comment\nvaw y = 10;"))
+# tokens = iter(lexer("vaw x = 5"))
 
-for token in tokens:
-    if token[0] == "KEYWORD":
-        match(token[1]):
-            case "vaw":
-                print(parse_variable_declaration(tokens))
-            case "functiwoon":
-                print(parse_function_declaration(tokens))
-    elif token[0] == "COMMENT":
-        continue
+ast = parse(tokens)
+
+print(ast)
 
